cb6d47f7bd75b946201ed7797c74f891073678fc
umfrancisco <francis.guitler@gmail.com>
2026-05-20 07:46:20 -0300

    First commit

---
+++ src/test/java/com/umfrancisco/estatisticas_criminais_project/EstatisticasCriminaisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.estatisticas_criminais_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatisticasCriminaisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
