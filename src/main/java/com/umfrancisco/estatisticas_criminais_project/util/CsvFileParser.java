1b4333e4bfd5aa8dc11dc1fef4a052d6aad18145
umfrancisco <francis.guitler@gmail.com>
2026-05-20 07:49:55 -0300

    Add CsvFileParser

---
+++ src/main/java/com/umfrancisco/estatisticas_criminais_project/util/CsvFileParser.java
@@ -0,0 +1,30 @@
+package com.umfrancisco.estatisticas_criminais_project.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CsvFileParser {
+	
+//	public static Repository read(Repository mapa, Cidade cidade) throws IOException {
+//		
+//		String fileName = cidade.getFileName();
+//		Path path = Paths.get(fileName);
+//		List<String> lines = Files.readAllLines(path);
+//		
+//		for (var line : lines) {
+//			String[] fields = line.split(",");
+//			Integer ano = Integer.parseInt(fields[0]);
+//			Integer homicidio = Integer.parseInt(fields[1]);
+//			Integer furto = Integer.parseInt(fields[2]);
+//			Integer roubo = Integer.parseInt(fields[3]);
+//			Integer frv = Integer.parseInt(fields[4]);
+//			mapa.add(new Crime(cidade, ano, homicidio, furto, roubo, frv));
+//		}
+//		
+//		return mapa;
+//	}
+	
+}
